da77065583a96f152043d9c5205341a6a4db6c75
tdoff <Nalr1meT190>
2019-11-17 17:25:25 +0600

    tinacms setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,23 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-tinacms',
+      options: {
+        plugins: [
+          "gatsby-tinacms-git",
+          'gatsby-tinacms-json',
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/data`,
+        name: 'data',
+      },
+    },
+    'gatsby-transformer-json',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,41 +8,48 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { useJsonForm } from 'gatsby-tinacms-json'
 
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
+  const dataQuery = useStaticQuery(graphql`
+    query MyQuery {
+      dataJson {
+        header {
           title
         }
+        rawJson
+        fileRelativePath
       }
     }
   `)
 
+  console.log({dataQuery})
+
+  const [data] = useJsonForm(dataQuery.dataJson, FormOptions);
+
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Header siteTitle={data.header.title} />
+      <main>{children}</main>
+      <footer>
+        footer
+      </footer>
     </>
   )
+}
+
+const FormOptions = {
+  fields: [
+    {
+      label: "Header title",
+      name: "rawJson.header.title",
+      description: 'Title of header',
+      component: "text",
+    },
+  ],
 }
 
 Layout.propTypes = {

8530690cb7061c46ecaea63a1a5dc054105a3c2f
Nurbek <nurbek-2395@mail.ru>
2019-11-26 20:20:43 +0600

    clean nav styles

--- src/components/categoriesNav/MobileCategory.jsx
+++ src/components/categoriesNav/MobileCategory.jsx
@@ -1,31 +1,31 @@
 import React from "react"
 import Img from "gatsby-image"
+import styled from "styled-components"
+
+const Item = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin: 0 3px;
+  width: 112px;
+  border: 1px solid rgb(234, 234, 236);
+  border-radius: 4px;
+  vertical-align: top;
+`
+
+const P = styled.p`
+  font-size: 11px;
+  font-weight: 500;
+  margin: 0;
+  line-height: "11px";
+`
+
 export default function MobileCategory({ category }) {
   return (
-    <li
-      style={{
-        display: "felx",
-        flexDirection: "column",
-        alignItems: "center",
-        textAlign: "center",
-        margin: "0 3px",
-        width: "112px",
-        border: "1px solid rgb(234, 234, 236)",
-        borderRadius: "4px",
-        verticalAlign: "top",
-      }}
-    >
+    <Item>
       <Img fixed={category.image.fixed} />
-      <p
-        style={{
-          fontSize: "11px",
-          fontWeight: 500,
-          margin: 0,
-          lineHeight: "11px",
-        }}
-      >
-        {category.name}
-      </p>
-    </li>
+      <P>{category.name}</P>
+    </Item>
   )
 }
--- src/components/categoriesNav/MobileNav.js
+++ src/components/categoriesNav/MobileNav.js
@@ -1,14 +1,24 @@
 import React from "react"
 import MobileCategory from "./MobileCategory"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  padding: 8px;
+`
+
+const List = styled.ul`
+  display: flex;
+  overflow-x: auto;
+`
 
 export default function MobileNav({ categories }) {
   return (
-    <div style={{ padding: "8px" }}>
-      <ul style={{ display: "flex", overflowX: "auto" }}>
+    <Wrapper>
+      <List>
         {categories.map(category => (
           <MobileCategory key={category.id} category={category} />
         ))}
-      </ul>
-    </div>
+      </List>
+    </Wrapper>
   )
 }

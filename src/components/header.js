1580545c9b63e09f1b0fbc0206e73dabd3ca937a
Nurbek <nurbek-2395@mail.ru>
2019-12-03 23:18:07 +0600

    Escape window object in header

--- src/components/header.js
+++ src/components/header.js
@@ -3,25 +3,32 @@ import { FiMenu, FiBookmark, FiShoppingBag, FiArrowLeft } from "react-icons/fi"
 import { Link } from "gatsby"
 
 const Header = () => {
-  const isMainPage = window.location.pathname === "/"
-  return (
-    <header>
-      <div>
-        {isMainPage ? (
-          <FiMenu className="icon" />
-        ) : (
-          <FiArrowLeft className="icon" onClick={() => window.history.back()} />
-        )}
-        <Link className="brand-name" to="/">
-          White Cat
-        </Link>
-      </div>
-      <div>
-        <FiBookmark className="icon" />
-        <FiShoppingBag className="icon" />
-      </div>
-    </header>
-  )
+  try {
+    const isMainPage = window.location.pathname === "/"
+    return (
+      <header>
+        <div>
+          {isMainPage ? (
+            <FiMenu className="icon" />
+          ) : (
+            <FiArrowLeft
+              className="icon"
+              onClick={() => window.history.back()}
+            />
+          )}
+          <Link className="brand-name" to="/">
+            White Cat
+          </Link>
+        </div>
+        <div>
+          <FiBookmark className="icon" />
+          <FiShoppingBag className="icon" />
+        </div>
+      </header>
+    )
+  } catch {
+    return null
+  }
 }
 
 export default Header
